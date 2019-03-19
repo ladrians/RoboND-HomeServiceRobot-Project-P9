0f7cd9b8d5a52a406adbc7d58e22d4e93fe6c7c8
Luciano Silveira <ladrians@gmail.com>
2019-03-18 23:41:40 -0300

    scripts updated

--- ros/src/ShellScripts/home_service.sh
+++ ros/src/ShellScripts/home_service.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-echo 'TODO'
+xterm -e "roslaunch ls_bot u.launch" &
+sleep 10
+xterm -e "roslaunch ls_bot view_navigation.launch" &
+sleep 3
+xterm -e "roslaunch ls_bot amcl.launch" &
+sleep 5
+xterm -e "rosrun add_markers add_markers.py" &
+sleep 5
+xterm -e "rosrun pick_objects pick_objects.py" &
+
--- ros/src/ShellScripts/wall_follower.sh
+++ ros/src/ShellScripts/wall_follower.sh
@@ -3,12 +3,8 @@
 echo ' '
 
 xterm -x "roslaunch ls_bot u.launch" &
-sleep 3 &&
+sleep 3
 xterm -x "roslaunch ls_bot teleop.launch" &
-sleep 3 &&
-
-echo ' '
-read -p 'Press any key to continue to mapping... ' -n1 -s
-
+sleep 3
 xterm -x "roslaunch ls_bot mapping.launch simulation:=true" &
 
